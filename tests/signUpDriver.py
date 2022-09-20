2f91d3bdf8126142207df96a636d6c9db71118fc
mr21Awesomeness <78995257+mr21Awesomeness@users.noreply.github.com>
2022-09-20 16:23:15 -0500

    Update Changes!!!

--- tests/signUpDriver.py
+++ tests/signUpDriver.py
@@ -1,22 +1,22 @@
 # This class is for the driver setup of signUp
-
-from selenium.webdriver import Chrome
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
 import time
 
 
 class SignUp:
-
     def __init__(self):
-
-        self.driver = Chrome()
+        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
         self.driver.get("http://localhost:5000")
-        self.driver.find_element_by_link_text("Sign up").click()
+        self.driver.find_element(By.LINK_TEXT, "Sign up").click()
 
     def _setup_user(self, username, password, checkPw):
-        self.driver.find_element_by_id("email").send_keys(username)
-        self.driver.find_element_by_id("password").send_keys(password)
-        self.driver.find_element_by_id("checkpassword").send_keys(checkPw)
-        self.driver.find_element_by_css_selector(".btn").click()
+        self.driver.find_element(By.ID, "email").send_keys(username)
+        self.driver.find_element(By.ID, "password").send_keys(password)
+        self.driver.find_element(By.ID, "checkpassword").send_keys(checkPw)
+        self.driver.find_element(By.CSS_SELECTOR, ".btn").click()
         self.driver.implicitly_wait(5)
 
     def sign_up(self, username, password):
@@ -28,7 +28,7 @@ class SignUp:
         url_current = self.driver.current_url
 
         # obtain error message if duplicate username happens
-        alert_info = self.driver.find_element_by_class_name("alert-info").text
+        alert_info = self.driver.find_element(By.CLASS_NAME, "alert-info").text
 
         SignUp.close(self)
 
@@ -38,7 +38,7 @@ class SignUp:
         # check the link to login page
 
         text = "Already have an account? Log in."
-        self.driver.find_element_by_link_text(text).click()
+        self.driver.find_element(By.LINK_TEXT, text).click()
         login_url = self.driver.current_url
         SignUp.close(self)
 
@@ -52,9 +52,9 @@ class SignUp:
         text1 = 'Passwords must match'
         text2 = 'Field must be between 8 and 80 characters long.'
         alert1 = self.driver.\
-            find_element_by_xpath("//*[text()=\"" + text1 + "\"]").text
+            find_element(By.XPATH, "//*[text()=\"" + text1 + "\"]").text
         alert2 = self.driver.\
-            find_element_by_xpath("//*[text()=\"" + text2 + "\"]").text
+            find_element(By.XPATH, "//*[text()=\"" + text2 + "\"]").text
         SignUp.close(self)
 
         return (alert1, alert2)
